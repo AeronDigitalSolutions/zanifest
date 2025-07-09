a1a960a6e0a6354a04d6d4200cd4eb2d3e541b94
Ankit <ankitsingh110198@gmail.com>
2025-07-09 18:59:46 +0530

    testing

--- src/components/home/CarInsuraceSection.tsx
+++ src/components/home/CarInsuraceSection.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Image from "next/image";
 import { FaArrowRight } from "react-icons/fa6";
 import styles from "@/styles/components/home/CarInsuranceSection.module.css";
-import {useRouter} from 'next/router';
+
 
 
 const INSURANCELIST = [
-  { name: "Family", image: require("@/assets/home/car/1.png"), onClick: handleFamily},
+  { name: "Family", image: require("@/assets/home/car/1.png"), },
   { name: "Critical Illness", image: require("@/assets/home/car/2.png") },
   {name: "Personal Accident", image: require("@/assets/home/car/3.png")},
   { name: "Car", image: require("@/assets/home/car/4.png") },
@@ -18,11 +18,7 @@ const INSURANCELIST = [
   { name: "Home Loan", image: require("@/assets/home/car/10.png") },
 ];
 function CarInsuraceSection() {
-  const router=useRouter();
-  
-const handleFamily=()=>{
-  router.push('./healthinsurance');
-}
+ 
   return (
     <div className={styles.cont}>
       <div className={styles.head}>
